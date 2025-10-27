08dcb965a0d435e5bf6bcbd548edc4eed20b69f4
LeeTaeYoung <xodud5080@naver.com>
2025-10-27 14:12:58 +0900

    member select api

--- src/main/java/jpabook/jpashop/api/MemberApiController.java
+++ src/main/java/jpabook/jpashop/api/MemberApiController.java
@@ -10,6 +10,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequiredArgsConstructor
@@ -20,6 +21,16 @@ public class MemberApiController {
     @GetMapping("/api/v1/members")
     public List<Member> membersV1(){
         return memberService.findMembers();
+    }
+
+    @GetMapping("/api/v2/members")
+    public Result membersV2(){
+        List<Member> findMembers = memberService.findMembers();
+        List<MemberDTO> collect = findMembers.stream()
+                .map(m -> new MemberDTO(m.getName()))
+                .collect(Collectors.toList());
+
+        return new Result(collect);
     }
 
     @PostMapping("/api/v1/members")
@@ -46,7 +57,16 @@ public class MemberApiController {
     }
 
     // select DTO
-    
+    @Data
+    @AllArgsConstructor
+    static class Result<T>{
+        private T data;
+    }
+    @Data
+    @AllArgsConstructor
+    static class MemberDTO{
+        private String name;
+    }
 
 
 
