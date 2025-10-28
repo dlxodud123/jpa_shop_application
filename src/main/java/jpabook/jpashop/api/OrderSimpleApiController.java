8615860e1c8e34f068c816760b6e7811625e95b3
LeeTaeYoung <xodud5080@naver.com>
2025-10-28 11:40:15 +0900

    simple select V3 - change DTO - fetch join

--- src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -14,7 +14,7 @@ import java.time.LocalDateTime;
 import java.util.List;
 import java.util.stream.Collectors;
 
-// xToOne(ManyToOne, OneToOne)
+// xToOne(ManyToOne, OneToOne) 
 // order
 // order -> member
 // order -> delivery
